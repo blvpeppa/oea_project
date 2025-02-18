4e2967eff80ae439622e9782fbe306ca5c64ce48
blvpeppa <blvpeppgram@gmail>
2025-02-18 07:35:21 +0200

    git commit -m

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Orthadox East Africa"/>
-  <h1>Welcome sano to Orthadox East Africa</h1>
+  <div class="container">
+  <header class="border-bottom lh-1 py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-4 pt-1">
+        <a class="link-secondary" href="#"> <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+          <img src="oca.png" alt="" srcset="" width="100" height="180">
+        </a></a>
+      </div>
+      <div class="col-4 text-center">
+        <a class="blog-header-logo text-body-emphasis text-decoration-none" href="index.html">Orthadox East Africa</a>
+      </div>
+      <div class="col-4 d-flex justify-content-end align-items-center">
+        <a class="link-secondary" href="#" aria-label="Search">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+        </a>
+        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+      </div>
+    </div>
+  </header>
+  <div class=" py-1 mb-3 border-bottom">
+    <nav class="nav nav-underline justify-content-between">
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#" >The Primate</a></li>
+        <li><a class="dropdown-item" href="#">The Holy Synod</a></li>
+        <li><a class="dropdown-item" href="#">Metropolitan Council</a></li>
+        <li><a class="dropdown-item" href="#">Chancery Staff</a></li>
+        <li><a class="dropdown-item" href="#">Departments</a></li>
+        <li><a class="dropdown-item" href="#">Boards, Offices, & Commissions</a></li>
+        <li><a class="dropdown-item" href="#">History & Archives</a></li>
+        <li><a class="dropdown-item" href="#">Documents & Policies</a></li></ul>
+
+          <a class="nav-item nav-link link-body-emphasis active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">About the OCE</a>
+      <a class="nav-item nav-link link-body-emphasis" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">News & Media</a>
+      <a class="nav-item nav-link link-body-emphasis" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">The Orthodox Faith</a>
+      <a class="nav-item nav-link link-body-emphasis" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Directories</a>
+      <a class="nav-item nav-link link-body-emphasis" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Work of the Church</a>
+    </nav>
+  </div>
+</div>
 </template>
 
 <script>
